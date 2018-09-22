4e558cbdf7bb9abedba71b9399e258771acdc8b1
Aloving <Ilegend2011@gmail.com>
2018-09-22 18:37:05 +0300

    Добавляет автоопределение времени дня

--- src/App.js
+++ src/App.js
@@ -25,6 +25,53 @@ const styles = theme => ({
 	},
 });
 
+const dayTimeIntervals = {
+	morning: [
+	    '0:00',
+	    '1:00',
+	    '2:00',
+	    '3:00',
+	    '4:00',
+	    '5:00',
+	    '6:00',
+	    '7:00',
+	    '8:00',
+		'9:00'
+	],
+	day: [
+		'10:00',
+		'11:00',
+		'12:00',
+		'13:00',
+		'14:00',
+		'15:00',
+		'16:00'
+	],
+	evening: [
+		'17:00',
+		'18:00',
+		'19:00',
+		'20:00',
+		'21:00',
+		'22:00',
+		'23:00'
+	],
+	getDayTime(hour) {
+		const hourString = `${hour}:00`;
+		if (this.evening.includes(hourString)) {
+			return 'evening';
+		}
+
+		if (this.day.includes(hourString)) {
+			return 'day';
+		}
+
+		if (this.morning.includes(hourString)) {
+			return 'morning';
+		}
+	}
+}
+
 const times = [
     '0:00',
     '1:00',
@@ -76,7 +123,7 @@ class AppComponent extends Component {
 		const anHour = newDate.getHours() + 1;
 
 		this.state = {
-			dayTime: 'morning',
+			dayTime: dayTimeIntervals.getDayTime(anHour),
 			text: DefaultText,
 			time: `${anHour}:00`
 		};
